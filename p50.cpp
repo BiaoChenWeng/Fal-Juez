c953f72bd7724266d90a4d80ef45f1fc5fb09964
Biao <113927864+BiaoChenWeng@users.noreply.github.com>
2022-11-24 12:27:33 +0100

    Add files via upload

---
+++ p50.cpp
@@ -0,0 +1,102 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+// Introduce más librerías si son necesarias
+using namespace std;
+
+struct tGanador {
+    bool saltar_partida;
+    int jugados;
+    
+};
+
+
+bool resuelveCaso();
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("datos.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+    std::ofstream out("datos.out");
+    auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    std::cout.rdbuf(coutbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
+
+
+tGanador jugador(vector<bool>const& v, int ini, int fin, int partido_actual, int p_buscado) {
+    tGanador b;
+    if (ini +1== fin ) {
+        b.jugados = 0;
+        b.saltar_partida = false;
+        return b;
+        
+    }
+    else if (ini + 2 == fin) {
+        if (!v[ini] && !v[ini+1]) {
+            
+            b.jugados = 0;
+            b.saltar_partida = true;
+        }
+        else if(!v[ini] || !v[ini+1]) {
+            b.jugados = 0;
+            b.saltar_partida = false;
+        }
+        else {
+            if (p_buscado >= partido_actual)
+                b.jugados=1;
+            b.saltar_partida = false;
+        }
+        return b;
+    }
+    else {
+        int m = (ini + fin) / 2;
+        tGanador a,c;
+        a=jugador(v, ini, m, partido_actual - 1, p_buscado);
+        c= jugador(v, m, fin, partido_actual - 1, p_buscado);
+
+        b.saltar_partida = a.saltar_partida && c.saltar_partida;
+        b.jugados = a.jugados + c.jugados;
+        if (!a.saltar_partida && !c.saltar_partida && p_buscado >= partido_actual) {
+            b.jugados++;
+        }
+
+        return b;
+
+    }
+}
+
+bool resuelveCaso() {
+    int entrada,partido;
+    cin >> entrada;
+    cin >> partido;
+    if (!std::cin)  // fin de la entrada
+        return false;
+    vector<bool >v(entrada);
+    string jug;
+    
+    for (int i = 0; i < v.size(); i++) {
+        cin >> jug;
+        v[i] = jug != "NP";
+
+    }
+
+    int k = log2(entrada);
+
+    cout << jugador(v, 0, v.size(), k, partido).jugados<<endl;
+
+
+    return true;
+}
+
---
+++ p57.cpp
@@ -0,0 +1,65 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+// Introduce más librerías si son necesarias
+using namespace std;
+
+
+
+bool resuelveCaso();
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("datos.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+    std::ofstream out("datos.out");
+    auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    std::cout.rdbuf(coutbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
+
+void permutacion(int m, int n,int etapa,vector<char>&v) {
+    for (char i = 'a'; i < 'a' + m; i++) {
+        v[etapa] = i;
+        if (n - 1 == etapa) {
+            for (int i = 0; i < n; i++) {
+                cout << v[i];
+            }
+            cout << endl;
+        }
+        else {
+            permutacion(m, n, etapa + 1, v);
+        }
+
+
+    }
+
+
+}
+
+
+bool resuelveCaso() {
+    int entrada,n;
+    cin >> entrada;
+    cin >> n;
+    if (!std::cin)  // fin de la entrada
+        return false;
+
+    vector<char>v(n);
+    permutacion(entrada, n, 0, v);
+    cout << "\n";
+
+    return true;
+}
+
