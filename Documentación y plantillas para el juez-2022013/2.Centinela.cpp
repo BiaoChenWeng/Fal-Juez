6b7306edfb577646bbc6a44376d342650d92edb6
Biao <113927864+BiaoChenWeng@users.noreply.github.com>
2023-02-10 16:48:50 +0100

    Add files via upload

---
+++ Documentación y plantillas para el juez-2022013/2.Centinela.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+// Introduce más librerías si son necesarias
+using namespace std;
+
+bool resuelveCaso();
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("datos.in");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+   std::ofstream out("datos.out");
+   auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   std::cout.rdbuf(coutbuf);
+   system("PAUSE");
+#endif
+   return 0;
+}
+
+
+
+bool resuelveCaso() {
+   
+   int entrada;
+   cin >> entrada;
+   if (caso especial)
+      return false;
+   
+ 
+   
+   // escribir sol
+   
+   return true;
+}
---
+++ Documentación y plantillas para el juez-2022013/3.CasosIlimitados.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+// Introduce más librerías si son necesarias
+using namespace std;
+
+bool resuelveCaso();
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("datos.in");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+   std::ofstream out("datos.out");
+   auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   std::cout.rdbuf(coutbuf);
+   system("PAUSE");
+#endif
+   return 0;
+}
+
+
+
+
+bool resuelveCaso() {
+   int entrada;
+   cin >> entrada;
+   if (!std::cin)  // fin de la entrada
+      return false;
+   
+ 
+   
+   return true;
+}
+
